e6540f5adf5d0e4007955e6ef292096d1f5ae06d
docktermj <michael@dockter.com>
2022-05-02 10:41:41 -0400

    #19 Change 'if' test

--- rootfs/app/docker-entrypoint.sh
+++ rootfs/app/docker-entrypoint.sh
@@ -2,12 +2,12 @@
 
 cp --recursive /opt/local-senzing/. /opt/senzing || true
 
-if [[ ! -z "${SENZING_DEPLOY_ETC_OPT_SENZING}" ]]; then
+if [[ "${SENZING_DEPLOY_ETC_OPT_SENZING}" == "true" ]]; then
     echo "Copying to /etc/opt/senzing"
     cp --recursive /etc/opt/local-senzing/. /etc/opt/senzing || true
 fi
 
-if [[ ! -z "${SENZING_DEPLOY_OPT_MICROSOFT}" ]]; then
+if [[ "${SENZING_DEPLOY_OPT_MICROSOFT}" == "true" ]]; then
     echo "Copying to /opt/microsoft"
     cp --recursive /opt/local-microsoft/. /opt/microsoft || true
 fi
